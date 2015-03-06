eda32cb6231ecc7eb3d74e74b437133ffc18df07
alexesba <alexesba@gmail.com>
2015-03-06 17:08:06 -0600

    Added spec for github redirect

--- test/unit/controllers/authController.js
+++ test/unit/controllers/authController.js
@@ -11,11 +11,21 @@ describe(TEST_NAME, function(){
 
   describe('google()', function(){
     it('should redirect to google.com/', function(done){
-      request.post('/auth/google').
-        send({ email: 'alexesba@gmail.com' })
+      request.get('/auth/google')
       .end(function(err, response){
         expect(response.status).to.equal(302);
         response.header['location'].should.include('google.com')
+        done();
+      });
+    });
+  });
+
+  describe('github()', function(){
+    it('should redirect to google.com/', function(done){
+      request.get('/auth/github')
+      .end(function(err, response){
+        expect(response.status).to.equal(302);
+        response.header['location'].should.include('github.com')
         done();
       });
     });
