102be58c53a4ef3b5692d79a19d8a0438d0b94c4
alexesba <alexesba@gmail.com>
2015-03-06 12:34:23 -0600

    Add test for channel model

---
+++ test/unit/models/Channel.js
@@ -0,0 +1,38 @@
+require('sails-test-helper');
+
+describe(TEST_NAME, function(){
+
+  var channelComplete = {};
+
+  beforeEach(function(done){
+    channelComplete = {
+      name: 'dex'
+    };
+    done();
+  });
+
+  describe('.create()', function(){
+
+    it('can\'t be created without a valid name', function(done){
+      Channel.create(channelComplete,function(err, channel){
+        expect(err.Errors.name).to.exist;
+        expect(err.Errors.name[0].message).
+          to.equal(Channel.validationMessages.name.minLength);
+        done();
+      });
+    });
+
+    it('successfuly create a channel with a valid name', function(done){
+      Channel.create({ name: 'Default'}, function(err, channel){
+        expect(err).to.not.exist;
+        expect(channel).to.have.property('name');
+        expect(channel.name).to.equal('Default');
+        done();
+      });
+    });
+
+  });
+
+
+
+});
